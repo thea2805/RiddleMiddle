6d36cceb28e02f8ae28a3abe2675c664d5c9b62d
Useless-4every1 <104831784+UselessDross@users.noreply.github.com>
2023-12-13 14:18:10 +0100

    readded the test basic navBar

---
+++ app/src/main/java/com/example/riddlemiddle/navTest/SearchScreen.kt
@@ -0,0 +1,46 @@
+package com.example.riddlemiddle.navTest
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun SearchScreen(navController: NavController) {
+    NavigationBarMediumTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(15.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxWidth()
+                        .height(250.dp)
+                        .padding(horizontal = 15.dp, vertical = 10.dp)
+                        .clip(MaterialTheme.shapes.large)
+                ) {
+
+                }
+                Text(
+                    "Search Screen",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(vertical = 20.dp)
+                )
+            }
+        }
+    }
+}
