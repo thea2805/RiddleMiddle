8d04759ce02d3b6cd0bbd7caf3a4176dd8f10b95
Useless-4every1 <104831784+UselessDross@users.noreply.github.com>
2023-12-14 17:03:52 +0100

    now the navigation between views works

---
+++ app/src/main/java/com/example/riddlemiddle/navTest/Screens.kt
@@ -0,0 +1,8 @@
+package com.example.riddlemiddle.navTest
+
+sealed class Screens(val route : String) {
+    object Start : Screens("StartScreen")
+    object RiddleAnswer : Screens("RiddleAnswer")
+    object Active : Screens("ActiveRiddle")
+
+}
